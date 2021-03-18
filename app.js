cab9d133f397499c79296119b1159d8176796521
Matthew Gazzera <genzu44@gmail.com>
2021-03-18 13:11:10 -0700

    gg finished

--- app.js
+++ app.js
@@ -5,8 +5,8 @@
 // let facts = {numPlanets: 8, yearNeptuneDiscovered: 1846};
 // let {numPlanets, yearNeptuneDiscovered} = facts;
 
-// console.log(numPlanets); // ?
-// console.log(yearNeptuneDiscovered); // ?
+console.log(numPlanets); // 8
+console.log(yearNeptuneDiscovered); // 1846
 
 // Object Destructuring 2
 // What does the following code return/print?
@@ -19,7 +19,7 @@
 
 // let {numPlanets, ...discoveryYears} = planetFacts;
 
-// console.log(discoveryYears); // ?
+console.log(discoveryYears); // {yearNeptuneDiscovered: 1846, yearMarsDiscovered: 1659}
 
 // Object Destructuring 3
 // What does the following code return/print?
@@ -28,17 +28,19 @@
 //   return `Your name is ${firstName} and you like ${favoriteColor}`;
 // }
 
-// getUserData({firstName: "Alejandro", favoriteColor: "purple"}) // ?
-// getUserData({firstName: "Melissa"}) // ?
-// getUserData({}) // ?
+getUserData({ firstName: 'Alejandro', favoriteColor: 'purple' }); // 'Your name is Alejandro and you like purple'
+getUserData({ firstName: 'Melissa' }); // 'Your name is Melissa and you like green'
+getUserData({}); // 'Your name is undefined and you like green'
+
 // Array Destructuring 1
 // What does the following code return/print?
 
 // let [first, second, third] = ["Maya", "Marisa", "Chi"];
 
-// console.log(first); // ?
-// console.log(second); // ?
-// console.log(third); // ?
+console.log(first); // "Maya"
+console.log(second); // "Marisa"
+console.log(third); // "Chi"
+
 // Array Destructuring 2
 // What does the following code return/print?
 
@@ -50,16 +52,18 @@
 //   "Brown paper packages tied up with strings"
 // ]
 
-// console.log(raindrops); // ?
-// console.log(whiskers); // ?
-// console.log(aFewOfMyFavoriteThings); // ?
+console.log(raindrops); // "Raindrops on roses"
+console.log(whiskers); // "whiskers on kittens"
+console.log(aFewOfMyFavoriteThings); // ["Bright copper kettles", "warm woolen mittens", "Brown paper packages tied up with strings"]
+
 // Array Destructuring 3
 // What does the following code return/print?
 
 // let numbers = [10, 20, 30];
 // [numbers[1], numbers[2]] = [numbers[2], numbers[1]]
 
-// console.log(numbers) // ?
+// console.log(numbers) // [10, 30, 20]
+
 // ES2015 Refactoring
 // In this exercise, you’ll refactor some ES5 code into ES2015.
 
@@ -73,6 +77,15 @@
 
 // var a = obj.numbers.a;
 // var b = obj.numbers.b;
+const obj = {
+     numbers: {
+          a: 1,
+          b: 2,
+     },
+};
+
+let { a, b } = obj.numbers;
+
 // ES2015 Object Destructuring
 // /* Write an ES2015 Version */
 // ES5 Array Swap
@@ -81,7 +94,9 @@
 // arr[0] = arr[1];
 // arr[1] = temp;
 // ES2015 One-Line Array Swap with Destructuring
-// /* Write an ES2015 Version */
+const arr = [1, 2];
+[arr[0], arr[1]] = [arr[1], arr[0]];
+
 // raceResults()
 // Write a function called raceResults which accepts a single array argument. It should return an object with the keys first, second, third, and rest.
 
@@ -103,3 +118,10 @@
 //     rest: ["David", "Pierre"]
 //   }
 // */
+
+const raceResults = ([first, second, third, ...rest]) => ({
+     first,
+     second,
+     third,
+     ...rest,
+});
